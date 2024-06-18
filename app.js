80f8f79273fcc402e654e7062a10f949079964c5
flikcards <168372038+flikcards@users.noreply.github.com>
2024-06-18 11:58:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+document.querySelector('.name').textContent = 'Aryan Motwani'
+document.querySelector('.title').textContent = 'Tech Enthusiast'
+document.querySelector('.description').textContent = 'Tech Enthusiast'
+
+// phone
+document.querySelector('.phone').href = 'tel:9372329966'
+
+
+document.querySelector('.insta').href = 'https://wa.me/+9372329966'
+document.querySelector('.whatsapp').href = 'https://www.instagram.com/flikcards?igsh=OHp2Yzlza2pkeHN1'
+document.querySelector('.fbook').href = 'https://www.instagram.com/flikcards?igsh=OHp2Yzlza2pkeHN1'
+document.querySelector('.snap').href = 'https://www.instagram.com/flikcards?igsh=OHp2Yzlza2pkeHN1'
+document.querySelector('.spotify').href = 'https://www.instagram.com/flikcards?igsh=OHp2Yzlza2pkeHN1'
